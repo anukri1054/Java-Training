cd6c3a9091da14438745cf79960da5ef1dc05f53
Anushka Kumari <167881753+anukri1054@users.noreply.github.com>
2026-06-04 15:43:34 +0530

    Create SumOfDigits.java

---
+++ SumOfDigits.java
@@ -0,0 +1,16 @@
+public class SumOfDigits {
+
+    public static int sumDigits(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        return (n % 10) + sumDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        int n = 324;
+
+        System.out.println("Sum of digits = " + sumDigits(n));
+    }
+}
