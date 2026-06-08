58dd4250c6858010aeb472225439d0b588bd6f27
Anushka Kumari <167881753+anukri1054@users.noreply.github.com>
2026-06-08 11:37:02 +0530

    Create Tree created.java

---
+++ Tree created.java
@@ -0,0 +1,30 @@
+public class TreeNode {
+    
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    
+    TreeNode(int x) {
+        this.val = x;
+        this.left = null;
+        this.right = null;
+    }
+
+     public static void main(String[] args){
+
+TreeNode root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+
+System.out.println("    " + root.val);
+        System.out.println("   / \\");
+        System.out.println("  " + root.left.val + "   " + root.right.val);
+        System.out.println(" / \\");
+        System.out.println(root.left.left.val + "   " + root.left.right.val);
+    
+}
+}
